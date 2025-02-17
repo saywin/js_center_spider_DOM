ef7972a8c171c6b4acea28cc8a896d03f8d51064
Artem <stilon670@gmail.com>
2025-02-17 12:13:05 +0200

    fix calculation top and left

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,5 +7,5 @@ const spiderHeight = spider.offsetHeight;
 const wallWidth = wall.offsetWidth;
 const wallHeight = wall.offsetHeight;
 
-spider.style.top = `${wallWidth / 2 - spiderWidth / 2}px`;
-spider.style.left = `${wallHeight / 2 - spiderHeight / 2}px`;
+spider.style.top = `${wallHeight / 2 - spiderHeight / 2}px`;
+spider.style.left = `${wallWidth / 2 - spiderWidth / 2}px`;
