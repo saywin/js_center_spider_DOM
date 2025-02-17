312987017fbc4ab72ced2a73716576f55fb1df9c
Artem <stilon670@gmail.com>
2025-02-17 12:04:38 +0200

    make build

---
+++ index.af3cdddd.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=e.querySelector(".spider"),o=t.offsetWidth,f=t.offsetHeight,c=e.offsetWidth,l=e.offsetHeight;t.style.top="".concat(c/2-o/2,"px"),t.style.left="".concat(l/2-f/2,"px");
+//# sourceMappingURL=index.af3cdddd.js.map
